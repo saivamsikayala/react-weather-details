9bb49c8386dba573ce3648b0330172bb5beaff97
saivamsikayala <ksaivamsi27@gmail.com>
2020-07-21 23:14:12 +0530

    Updates

---
+++ precache-manifest.4fcad897eef98667b6fce21c1794291d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f02ba9fa1001ff22e263c6e1b58211",
+    "url": "/react-weather-details/index.html"
+  },
+  {
+    "revision": "359fd94aa5c0dd1cbe19",
+    "url": "/react-weather-details/static/css/main.de371f6f.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/react-weather-details/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-details/static/js/2.07144284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359fd94aa5c0dd1cbe19",
+    "url": "/react-weather-details/static/js/main.6bfd1e75.chunk.js"
+  },
+  {
+    "revision": "bc7493a358a719e71e9f",
+    "url": "/react-weather-details/static/js/runtime-main.25d21b14.js"
+  },
+  {
+    "revision": "bbd925ef91621cc4a8b5fcdd54ccc17d",
+    "url": "/react-weather-details/static/media/cloudy.bbd925ef.jpg"
+  },
+  {
+    "revision": "6f8dcfa6eb323bba21a65f1b59afdb2c",
+    "url": "/react-weather-details/static/media/cold.6f8dcfa6.jpg"
+  },
+  {
+    "revision": "035ace08fc3be13187b5b9e12a725431",
+    "url": "/react-weather-details/static/media/rain.035ace08.jpg"
+  },
+  {
+    "revision": "14f2cac148fd05a1a8b67f68f8d08fae",
+    "url": "/react-weather-details/static/media/warm.14f2cac1.jpg"
+  }
+]);
